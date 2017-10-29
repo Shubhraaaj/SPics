65a8e3e719c6a4402be3343f2639327e9b908995
Shubhraj Prasad Singh <ashu.shubhraj@gmail.com>
2017-10-29 22:20:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubhraj/spics/activity/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.shubhraj.spics.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import com.example.shubhraj.spics.R;
+import com.example.shubhraj.spics.adapter.GalleryAdapter;
+import com.example.shubhraj.spics.app.AppController;
+import com.example.shubhraj.spics.model.Image;
+
+public class MainActivity extends AppCompatActivity
+{
+    private static final java.lang.String SLIDE_TAG = "slide-show";
+    private String TAG = MainActivity.class.getSimpleName();
+    private static final String endpoint = "https://luxuri.000webhostapp.com/json3.html";
+    private ArrayList<Image> images;
+    private ProgressBar mProgressBar;
+    private GalleryAdapter mAdapter;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
+        images = new ArrayList<>();
+        mAdapter = new GalleryAdapter(getApplicationContext(), images);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.addOnItemTouchListener(new GalleryAdapter.RecyclerTouchListener(getApplicationContext(), recyclerView, new GalleryAdapter.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("images", images);
+                bundle.putInt("position", position);
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                SlideShowDialogFragment newFragment = SlideShowDialogFragment.newInstance();
+                newFragment.setArguments(bundle);
+                newFragment.show(ft, SLIDE_TAG);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+        fetchImages();
+    }
+
+    private void fetchImages()
+    {
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        JsonArrayRequest req = new JsonArrayRequest(endpoint,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        mProgressBar.setVisibility(View.GONE);
+
+                        images.clear();
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject object = response.getJSONObject(i);
+                                Image image = new Image();
+                                image.setName(object.getString("name"));
+
+                                JSONObject url = object.getJSONObject("url");
+                                image.setSmall(url.getString("small"));
+                                image.setMedium(url.getString("medium"));
+                                image.setLarge(url.getString("large"));
+                                image.setTimestamp(object.getString("timestamp"));
+
+                                images.add(image);
+
+                            } catch (JSONException e) {
+                                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                            }
+                        }
+
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+                mProgressBar.setVisibility(View.GONE);
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/shubhraj/spics/activity/SlideShowDialogFragment.java
@@ -0,0 +1,146 @@
+package com.example.shubhraj.spics.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.shubhraj.spics.R;
+import com.example.shubhraj.spics.model.Image;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Shubhraj on 29-10-2017.
+ */
+
+public class SlideShowDialogFragment extends DialogFragment
+{
+    private String TAG = SlideShowDialogFragment.class.getSimpleName();
+    private ArrayList<Image> images;
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private TextView labelCount, labelTitle, labelDate;
+    private int selectedPosition = 0;
+
+    static SlideShowDialogFragment newInstance() {
+        SlideShowDialogFragment f = new SlideShowDialogFragment();
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_image_slider, container, false);
+        viewPager = (ViewPager) v.findViewById(R.id.viewpager);
+        labelCount = (TextView) v.findViewById(R.id.lbl_count);
+        labelTitle = (TextView) v.findViewById(R.id.title);
+        labelDate = (TextView) v.findViewById(R.id.date);
+
+        images = (ArrayList<Image>) getArguments().getSerializable("images");
+        selectedPosition = getArguments().getInt("position");
+
+        Log.e(TAG, "position: " + selectedPosition);
+        Log.e(TAG, "images size: " + images.size());
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        setCurrentItem(selectedPosition);
+
+        return v;
+    }
+
+    private void setCurrentItem(int position) {
+        viewPager.setCurrentItem(position, false);
+        displayMetaInfo(selectedPosition);
+    }
+
+    //	page change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            displayMetaInfo(position);
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    private void displayMetaInfo(int position) {
+        labelCount.setText((position + 1) + " of " + images.size());
+
+        Image image = images.get(position);
+        labelTitle.setText(image.getName());
+        labelDate.setText(image.getTimestamp());
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(android.support.v4.app.DialogFragment.STYLE_NORMAL, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+    }
+
+    //	adapter
+    public class MyViewPagerAdapter extends PagerAdapter {
+
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = layoutInflater.inflate(R.layout.image_fullscreen, container, false);
+
+            ImageView imageViewPreview = (ImageView) view.findViewById(R.id.image_preview);
+
+            Image image = images.get(position);
+
+            Glide.with(getActivity())
+                    .load(image.getLarge())
+                    .thumbnail(0.5f)
+                    .into(imageViewPreview);
+
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == ((View) obj);
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+}
+
